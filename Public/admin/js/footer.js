0e34eec1a2942ff903c2e61e69a154acf9d7a68e
KeruMx <eemmanuel.lunaa@gmail.com>
2020-01-22 17:41:35 -0600

    Index preview terminado

--- Public/admin/js/footer.js
+++ Public/admin/js/footer.js
@@ -4,7 +4,7 @@ let htmlfoot = '        <footer class="text-center">\n' +
     '                <br>\n' +
     '                Desarrollado por: <a href="https://github.com/KeruMx" target="_blank" class="text-muted"><img src="/Checador/Public/Images/github128px.png" alt="Github KeruMx" height="13px">/KeruMx</a>\n' +
     '            </p>\n' +
-    '            <div id="img"> <img src="../../Images/cecy.jpeg\" alt="Logo cecyte" height="70" class="" style="margin-top:20px; margin-bottom: 20px; "> </div>\n' +
+    '            <div id="img"> <img src="/checador-node/Public/Images/cecy.jpeg\" alt="Logo cecyte" height="70" class="" style="margin-top:20px; margin-bottom: 20px; "> </div>\n' +
     '            <hr>\n' +
     '        </footer>\n' +
     '\n'
